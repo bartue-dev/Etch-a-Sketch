93a4abbdcf4c36b3c822f3c76776373fd87875a1
Bartue <172117404+bartue-dev@users.noreply.github.com>
2024-08-12 20:01:27 +0800

    Add event listener to the input type range. Add .clientWidth method then devide it to sliderValue to determine the height and width of the squares. Multiplies the sliderValue from the for loop to generate more squares inside the container. Remove the eventListner and wraped the whole code into a arrow function then pass the function to the seperate input event listener and window load event listener to display the squares rigth before the page loads

--- script.js
+++ script.js
@@ -1,84 +1,112 @@
-const main = document.querySelector("main");
 
-for(i = 0; i < 1500; i++){
-  const squares = document.createElement("div");
-  squares.classList.add("square")
-  main.appendChild(squares);
+  const main = document.querySelector("main");
+  const slider = document.querySelector(".input-slider");
 
- 
-  const colorPicker = document.querySelector("#input-color-picker");
-  colorPicker.addEventListener("input", () => {
-    console.log(colorPicker.value)
-    const colorValue = colorPicker.value
-  
-    squares.addEventListener("mouseover", e => {
-      e.preventDefault();
-      if (e.buttons === 1) {
-        squares.style.backgroundColor = `${colorValue}`;
-      }
-    });
+  const sliderDOM = () => {
     
-  })
-
-  //default mouse color if not select any color button
-  const defaultMouseColor = () => {
-    squares.addEventListener("mouseover", e => {
-      e.preventDefault();
-      if (e.buttons === 1) {
-        squares.style.backgroundColor = "black";
+      const sliderValue = parseInt(slider.value);
+      const mainWidth = main.clientWidth;
+      console.log(sliderValue);
+  
+      main.innerHTML = "";
+  
+      const squareSize = Math.floor(mainWidth / sliderValue);
+  
+      main.style.cssText = `
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      align-items: center;
+      `
+  
+      for(i = 0; i < sliderValue * sliderValue ; i++){
+        const squares = document.createElement("div");
+        squares.classList.add("square");
+  
+        squares.style.cssText = `
+        flex-basis: ${squareSize}px;
+        height: ${squareSize}px;
+        background-color: white;
+        margin: 0;
+        `
+  
+        main.appendChild(squares);
+      
+      
+        const colorPicker = document.querySelector("#input-color-picker");
+        colorPicker.addEventListener("input", () => {
+          console.log(colorPicker.value)
+          const colorValue = colorPicker.value
+        
+          squares.addEventListener("mouseover", e => {
+            e.preventDefault();
+            if (e.buttons === 1) {
+              squares.style.backgroundColor = `${colorValue}`;
+            }
+          });
+          
+        })
+      
+        //default mouse color if not select any color button
+        const defaultMouseColor = () => {
+          squares.addEventListener("mouseover", e => {
+            e.preventDefault();
+            if (e.buttons === 1) {
+              squares.style.backgroundColor = "black";
+            }
+          });
+        }
+      
+        //if button prism selected it assign the random color into the cursor
+        const prism = () => {
+          const prism = document.querySelector(".btn-prism");
+      
+          prism.addEventListener("click", () => {
+            squares.addEventListener("mouseover", e => {
+              e.preventDefault();
+              if (e.buttons === 1) {
+                squares.style.backgroundColor = `${getRandomRgb()}`;
+              }
+            });
+          })
+        }
+      
+        //if pen button is selected it assign a black color into the cursor
+        const pen = () => {
+          const pen = document.querySelector(".btn-pen");
+      
+          pen.addEventListener("click", () =>{
+            squares.addEventListener("mouseover", e => {
+              e.preventDefault();
+              if (e.buttons === 1) {
+                squares.style.backgroundColor = "black";
+              }
+            });
+          })
+        }
+        
+        //if eraser button is selected it will assign a white color into the cursor
+        const eraser = () => {
+          const eraser = document.querySelector(".btn-eraser");
+      
+          eraser.addEventListener("click", () => {
+            squares.addEventListener("mouseover", e => {
+              e.preventDefault();
+              if (e.buttons === 1) {
+                squares.style.backgroundColor = "white";
+              }
+            });
+          })
+        }
+      
+        //call all the buttons function as well as the default function
+        defaultMouseColor();
+        prism();
+        pen();
+        eraser();
       }
-    });
-  }
-
-  //if button prism selected it assign the random color into the cursor
-  const prism = () => {
-    const prism = document.querySelector(".btn-prism");
-
-    prism.addEventListener("click", () => {
-      squares.addEventListener("mouseover", e => {
-        e.preventDefault();
-        if (e.buttons === 1) {
-          squares.style.backgroundColor = `${getRandomRgb()}`;
-        }
-      });
-    })
-  }
-
-  //if pen button is selected it assign a black color into the cursor
-  const pen = () => {
-    const pen = document.querySelector(".btn-pen");
-
-    pen.addEventListener("click", () =>{
-      squares.addEventListener("mouseover", e => {
-        e.preventDefault();
-        if (e.buttons === 1) {
-          squares.style.backgroundColor = "black";
-        }
-      });
-    })
   }
   
-  //if eraser button is selected it will assign a white color into the cursor
-  const eraser = () => {
-    const eraser = document.querySelector(".btn-eraser");
-
-    eraser.addEventListener("click", () => {
-      squares.addEventListener("mouseover", e => {
-        e.preventDefault();
-        if (e.buttons === 1) {
-          squares.style.backgroundColor = "white";
-        }
-      });
-    })
-  }
-
-  //call all the buttons function as well as the default function
-  defaultMouseColor();
-  prism();
-  pen();
-  eraser();
-}
-
 function getRandomRgb(){
   let num = Math.round(0xffffff * Math.random());
   let r = num >> 16;
@@ -86,6 +114,11 @@ function getRandomRgb(){
   let b = num & 255;
   return `rgb(${r},${g},${b})`;
 }
+
+slider.addEventListener("change", sliderDOM);
+window.addEventListener("load", sliderDOM)
+
+
 
 
 
